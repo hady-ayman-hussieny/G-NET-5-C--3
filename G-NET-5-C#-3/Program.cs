70dad25c918c30fecd052a2261047e45b62a9842
Hady Ayman <dodo143131@gmail.com>
2026-02-26 21:58:14 +0200

    Add project files.

---
+++ G-NET-5-C#-3/Program.cs
@@ -0,0 +1,10 @@
+﻿public class Program
+{
+     static void Main(string[] args)
+    {
+       //Q1: What will this print and explain what happens ?
+        double d = 9.99;
+        int x = (int)d;
+        Console.WriteLine(x);//9
+    }
+}
